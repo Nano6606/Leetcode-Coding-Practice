72e097f700d5aa5165235371cdc67b5664cb886c
Nano6606 <129884507+Nano6606@users.noreply.github.com>
2024-02-07 21:39:16 +0530

    Update Palindrome number.cpp

--- Palindrome number.cpp
+++ Palindrome number.cpp
@@ -7,8 +7,6 @@ SOLUTION:-
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // Special cases:
-        // If x is negative or ends with 0 but is not 0, it can't be a palindrome.
         if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
@@ -19,9 +17,6 @@ public:
             reversed = reversed * 10 + digit;
             x /= 10;
         }
-
-        // For even-length numbers, x will equal reversed.
-        // For odd-length numbers, x will be the middle digit, so we need to remove it.
         return x == reversed || x == reversed / 10;
     }
 };
